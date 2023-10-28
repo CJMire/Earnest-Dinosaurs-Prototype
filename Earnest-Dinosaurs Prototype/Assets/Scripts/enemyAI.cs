e83210074c709398ce8d555beac025f2e38deedf
Chayathorn Kanjanapetcharat <129718237+C-KJN@users.noreply.github.com>
2023-10-28 11:20:03 +0700

    Implemented enemy navigation
    - Using lecture code as a reference.
    - Added NavMeshAgent component to the enemy.
    - Setup a test target for enemy to move to.
    - Added faceTarget() which will make enemy continued looking at target within the stopping distance.

--- Earnest-Dinosaurs Prototype/Assets/Scripts/enemyAI.cs
+++ Earnest-Dinosaurs Prototype/Assets/Scripts/enemyAI.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour
 {
+    [Header("----- Enemy's Components ------")]
+    [SerializeField] NavMeshAgent navAgent;
+    [SerializeField] Transform targetPosition;  //Player is not yet implemented so adding transform position for testing 
+
+    [Header("----- Enemy's Stats ------")]
+    [SerializeField] int facingSpeed;
+
+    Vector3 targetDirection;        
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,25 @@ public class enemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Get direction of the target 
+        targetDirection = targetPosition.position - transform.position;
+
+        //When target is within the stopping distance, continue facing the target 
+        if(navAgent.remainingDistance < navAgent.stoppingDistance)
+        {
+            faceTarget();
+        }
+
+        //Set the target position as destination 
+        navAgent.SetDestination(targetPosition.position);
+    }
+
+    void faceTarget()
+    {
+        //Get rotation to the target 
+        Quaternion faceRotation = Quaternion.LookRotation(targetDirection);
+
+        //Rotate to the target using lerp with set up speed
+        transform.rotation = Quaternion.Lerp(transform.rotation, faceRotation, Time.deltaTime * facingSpeed);
     }
 }
